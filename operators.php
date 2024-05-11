e274be9c2d029bcda32dc99279ffb29e604a4a0b
nagphoo12.phoobaby7 <nangphoo12.phoobaby7@gmail.com>
2024-05-11 14:50:25 +0630

    operators

---
+++ operators.php
@@ -0,0 +1,26 @@
+<?php
+// php assignment operator
+$x=10;
+$y=2;
+echo $x *= $y;
+echo"<br>";
+echo $x /= $y;
+echo"<br>";
+echo $x %= $y;
+echo"<br>";
+//comparison operator
+$p=10;
+$s=20;
+if( $p <= $s){
+    echo"true";
+}else
+{ echo"false";}
+echo"<br>";
+//php decrement
+$i =11;
+echo $i--;
+echo"<br>";
+$e = 10;
+echo --$i;
+
+?>
