2c0858e41b3bc302669c088a679f766dcfaa6073
nangphoo12 <phoobaby7@gmail.com>
2024-05-20 16:11:38 +0630

    Add files via upload

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre/>";
+$data= array("apple" ,"orange" ,"grape" ,"mango" ,"pineapple" ,"lemon" ,"kiwi" ,"watermelon" ,"coconut" ,"redberry" );
+var_dump($data);
+$data[9]= "blueberry";
+var_dump($data);
+echo "$data[6]";
+?>
